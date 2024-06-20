faf647edfdb8738290b68fc62c0a4af88cb0dfb7
Daniel <161477598+DanielFS22@users.noreply.github.com>
2024-06-19 21:28:26 -0300

    Update dino.js

--- dino.js
+++ dino.js
@@ -7,8 +7,8 @@ import {
 const dinoElem = document.querySelector("[data-dino]") // Seleciona o elemento do dinossauro
 const JUMP_SPEED = 0.45 // Velocidade do salto
 const GRAVITY = 0.0015 // Gravidade que puxa o dinossauro para baixo
-const DINO_FRAME_COUNT = 2 // Número de frames para a animação de corrida
-const FRAME_TIME = 100 // Tempo de cada frame da animação
+const DINO_FRAME_COUNT = 10 // Número de frames para a animação de corrida
+const FRAME_TIME =  54// Tempo de cada frame da animação
 
 let isJumping // Variável para verificar se o dinossauro está pulando
 let dinoFrame // Frame atual da animação de corrida
@@ -39,19 +39,19 @@ export function getDinoRect() {
 
 // Função para definir a imagem do dinossauro quando ele perde
 export function setDinoLose() {
-  dinoElem.src = "imgs/dino-lose.png"
+  dinoElem.src = "imgs/viviTriste.png"
 }
 
 // Função para controlar a animação de corrida
 function handleRun(delta, speedScale) {
   if (isJumping) {
-    dinoElem.src = `imgs/dino-stationary.png`
+    dinoElem.src = `imgs/viviJump.png`
     return
   }
 
   if (currentFrameTime >= FRAME_TIME) {
     dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
-    dinoElem.src = `imgs/dino-run-${dinoFrame}.png`
+    dinoElem.src = `imgs/sprite_0${dinoFrame}.png`
     currentFrameTime -= FRAME_TIME
   }
   currentFrameTime += delta * speedScale
